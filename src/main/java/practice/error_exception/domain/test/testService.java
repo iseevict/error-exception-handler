ad072fabd3902b44d2f9c1d4fd1004d0df12faa1
isee <stpark0509@gmail.com>
2025-01-14 09:51:09 +0900

    GMS 프로젝트에서 사용할 에러처리 기법 테스트 - 쓰레드 네이밍 추가

--- src/main/java/practice/error_exception/domain/test/testService.java
+++ src/main/java/practice/error_exception/domain/test/testService.java
@@ -10,11 +10,22 @@ import practice.error_exception.global.exception.object.ServiceException;
 @Service
 @Slf4j
 public class testService {
+    @Scheduled(fixedDelay = 10000)
+    public void LogTest() {
+        Thread.currentThread().setName("EmfoGMS-LogTest");
+        log.error("This is the test to check if Logs are logged in the gmsLog file");
+    }
 
     @Scheduled(fixedDelay = 10000)
-    public void logTest() {
-        log.error("test");
+    public void CustomExceptionsTest() {
+        Thread.currentThread().setName("EmfoGMS-CustomExceptions");
+        throw new testHandler("TEST001", "Exception for Custom Exception Test");
+    }
 
-        throw new testHandler("test", "test");
+    @Scheduled(fixedDelay = 10000)
+    public void AllExceptionsTest() {
+        Thread.currentThread().setName("EmfoGMS-AllExceptions");
+        log.error("Exception for All Exception Test.");
+        throw new IllegalArgumentException();
     }
 }
